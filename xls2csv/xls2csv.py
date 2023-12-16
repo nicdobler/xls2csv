b407ba37fbfc1dd059d52ee0ae1e56d91aab54ad
Nicolás Dobler <nicolas.dobler@gmail.com>
2023-12-16 23:08:29 +0100

    Receives outputfilename, quote strings

--- xls2csv/xls2csv.py
+++ xls2csv/xls2csv.py
@@ -3,14 +3,16 @@ Convierte el excel pasado como parametro en un csv
 """
 # importing pandas module
 import pandas as pd
+import csv
 import sys
 
 # input excel file path
 inputExcelFile = sys.argv[1]
+outputCSV = sys.argv[2]
 print(f"Generating csv from {inputExcelFile}")
 
 # Reading an excel file
 excelFile = pd.read_excel(inputExcelFile)
 
 # Converting excel file into CSV file
-excelFile.to_csv(f"{inputExcelFile}.csv", index=None, header=True)
+excelFile.to_csv(f"{outputCSV}.csv", index=None, header=True, quoting=csv.QUOTE_NONNUMERIC)
