539460ab55bf2f0169e99a410cc7b4c3e3806cb6
Nicolás Dobler <nicolas.dobler@gmail.com>
2023-12-17 01:43:08 +0100

    Prepare to read multiple files

--- xls2csv/xls2csv.py
+++ xls2csv/xls2csv.py
@@ -1,18 +1,44 @@
 """
-Convierte el excel pasado como parametro en un csv
+Convierte los excels pasados como parametros en un csv
+agregando el nombre del fichero como primer elemento.
 """
 # importing pandas module
 import pandas as pd
 import csv
 import sys
 
-# input excel file path
-inputExcelFile = sys.argv[1]
-outputCSV = sys.argv[2]
-print(f"Generating csv from {inputExcelFile}")
+xlsList = []
 
-# Reading an excel file
-excelFile = pd.read_excel(inputExcelFile)
+first = True
+# iterate over excel files
+for i in range(1, len(sys.argv)):
+    inputExcelFile = sys.argv[i]
+    print(f"Reading {inputExcelFile}")
+
+    # Reading an excel file
+    excelFile = pd.read_excel(inputExcelFile)
+    print('Dropping headers')
+    if first:
+        excelFile = excelFile.drop(excelFile.index[[0, 6]])
+        first = False
+    else:
+        excelFile = excelFile.drop(excelFile.index[[0, 7]])
+    excelFile['Account'] = inputExcelFile
+    xlsList.append(excelFile)
+
+    print(f"Done with {inputExcelFile}")
+
+print("Done with reading. Merging dataframes.")
+
+merged = pd.DataFrame()
+
+for f in xlsList:
+    merged = merged.append(f, ignore_index=True)
+
+print("Writing CSV")
 
 # Converting excel file into CSV file
-excelFile.to_csv(f"{outputCSV}.csv", index=None, header=True, quoting=csv.QUOTE_NONNUMERIC)
+merged.to_csv("output.csv", index=None, header=True,
+              quoting=csv.QUOTE_NONNUMERIC)
+
+print("Done")
