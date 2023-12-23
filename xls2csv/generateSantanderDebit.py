e9db7efaae0c32dc1de373a05272d2896c6819fd
Nicolás Dobler <nicolas.dobler@gmail.com>
2023-12-23 18:31:24 +0100

    Eliminado reference que no vale para nada

--- xls2csv/generateSantanderDebit.py
+++ xls2csv/generateSantanderDebit.py
@@ -85,7 +85,7 @@ for inputExcelFile in glob.iglob(path + "/D*.xls"):
     csvFile["amount"]=excelFile["IMPORTE EUR"].abs()
     csvFile["trxType"]=excelFile["IMPORTE EUR"].apply(lambda x: "credit" if x >= 0 else "debit").astype('category')
     csvFile["category"]=""
-    csvFile["reference"]=gen_transaction_id(excelFile['CONCEPTO'], excelFile['IMPORTE EUR'], excelFile['FECHA VALOR'])
+    csvFile["reference"]=""
     csvFile["labels"]=accountName
     csvFile['memo']=excelFile['CONCEPTO'].apply(get_memo)
 
@@ -108,7 +108,7 @@ output=f'{path}/Debit.csv'
 print("Writing CSV to " + output)
 
 # Converting excel file into CSV file
-csvFile.to_csv(output, index=None, header=False, quoting=csv.QUOTE_NONE, date_format='%m/%d/%Y')
+merged.to_csv(output, index=None, header=False, quoting=csv.QUOTE_NONE, date_format='%m/%d/%Y')
 
 if os.access(output, os.R_OK):
     print("File written ok")
