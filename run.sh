c52e0d328452280d7fb03c4e016f4b4b1a8ab16c
Nicolás Dobler <nicolas.dobler@gmail.com>
2023-12-23 19:27:06 +0100

    Add ing format

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ if [[ "$1" == "debit" ]]; then
   poetry run python xls2csv/generateSantanderDebit.py /inout
 elif [[ "$1" == "credit" ]]; then
   poetry run python xls2csv/generateSantanderCredit.py /inout
+elif [[ "$1" == "ing" ]]; then
+  poetry run python xls2csv/generateIng.py /inout
 fi
---
+++ xls2csv/generateIng.py
@@ -0,0 +1,89 @@
+"""
+Convierte los excels pasados como parametros en un csv
+agregando el nombre del fichero como primer elemento.
+"""
+# importing pandas module
+import pandas as pd
+import csv
+import sys
+import glob
+import os
+import re
+
+
+def get_payee(concepto):
+    patrones = [
+        r'^Nomina recibida (.*)$',
+        r'^Pago en (.*)$',
+        r'^Transferencia emitida a (.*)$',
+        r'^Transferencia recibida de (.*)$',
+    ]
+    payee = concepto
+    for p in patrones:
+        x = re.findall(p, concepto)
+        if x:
+            payee = x[0]
+            break
+    return payee.replace(',', ' ')
+
+
+xlsList = []
+
+path = sys.argv[1]
+
+# iterate over excel files
+for inputExcelFile in glob.iglob(path + "/Movements.xls"):
+
+    accountName = "Ing Ele"
+
+    print(f"Reading {inputExcelFile}")
+
+    excelFile = pd.read_excel(path + "Movements.xls", header=4, engine="xlrd")
+    print(f'Columns: {excelFile.columns}')
+    print(f'Columns: {excelFile.dtypes}')
+    print(f'Readed {excelFile.size} rows')
+
+    print("Converting")
+    csvFile = pd.DataFrame()
+    csvFile["trxDate"] = pd.to_datetime(excelFile['F. VALOR'])
+    csvFile["payee"] = excelFile['DESCRIPCIÓN'].apply(get_payee)
+    csvFile["originalpayee"] = excelFile["DESCRIPCIÓN"].str.replace(",", "")
+    csvFile["amount"] = excelFile["IMPORTE (€)"].abs()
+    csvFile["trxType"] = csvFile["amount"].apply(
+        lambda x: "credit" if x >= 0 else "debit").astype('category')
+    csvFile["category"] = ""
+    csvFile["reference"] = ""
+    csvFile["labels"] = accountName
+    csvFile['memo'] = excelFile[['CATEGORÍA', 'SUBCATEGORÍA']].\
+        agg('/'.join, axis=1) \
+        if excelFile["DESCRIPCIÓN"].str.startswith('Pago en') \
+        else ""
+
+    # adding to converted files list
+    xlsList.append(csvFile)
+
+if xlsList:
+    print("Done with reading. Merging dataframes.")
+else:
+    if os.access(path, os.R_OK):
+        listFiles = os.listdir(path)
+    else:
+        listFiles = "Path is not a directory"
+    print(f'No files read in {path}. Contents: {listFiles}')
+    exit(1)
+
+merged = pd.concat(xlsList)
+
+output = f'{path}/Ing.csv'
+print("Writing CSV to " + output)
+
+# Converting excel file into CSV file
+merged.to_csv(output, index=None, header=False, quoting=csv.QUOTE_NONE,
+              date_format='%m/%d/%Y')
+
+if os.access(output, os.R_OK):
+    print("File written ok")
+else:
+    print("Something went wrong")
+
+print("Done")
