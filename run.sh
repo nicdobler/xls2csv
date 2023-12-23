6957bffd65589a603a2b322b1616f1ca29e950a3
Nicolás Dobler <nicolas.dobler@gmail.com>
2023-12-23 18:31:36 +0100

    menos verbose

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo Getting transactions using $1. Folder contents:
-ls /inout
+echo Getting transactions using $1.
 
 if [[ "$1" == "debit" ]]; then
   poetry run python xls2csv/generateSantanderDebit.py /inout
