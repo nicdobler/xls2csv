c99540e1e3495d50cdcfc7a93b13dd41789a5f7c
Nicolás Dobler <nicolas.dobler@gmail.com>
2023-12-24 00:32:41 +0100

    Added description

--- xls2csv/generateIng.py
+++ xls2csv/generateIng.py
@@ -12,19 +12,31 @@ import re
 
 
 def get_payee(concepto):
+    item = str(concepto)
     patrones = [
         r'^Nomina recibida (.*)$',
         r'^Pago en (.*)$',
         r'^Transferencia emitida a (.*)$',
         r'^Transferencia recibida de (.*)$',
     ]
-    payee = concepto
+    payee = item
     for p in patrones:
-        x = re.findall(p, concepto)
+        x = re.findall(p, item)
         if x:
             payee = x[0]
             break
     return payee.replace(',', ' ')
+
+
+def getDesc(series):
+    esPago = str(series['DESCRIPCIÓN']).startswith('Pago en')
+    if esPago:
+        cate = str(series['CATEGORÍA'])
+        subcat = str(series['SUBCATEGORÍA'])
+        desc = cate + '/' + subcat
+        return desc.replace(',', '')
+    else:
+        return ""
 
 
 xlsList = []
@@ -38,7 +50,8 @@ for inputExcelFile in glob.iglob(path + "/Movements.xls"):
 
     print(f"Reading {inputExcelFile}")
 
-    excelFile = pd.read_excel(path + "Movements.xls", header=4, engine="xlrd")
+    excelFile = pd.read_excel(inputExcelFile, header=4, engine="xlrd")
+    excelFile = excelFile[:-1]
     print(f'Columns: {excelFile.columns}')
     print(f'Columns: {excelFile.dtypes}')
     print(f'Readed {excelFile.size} rows')
@@ -48,16 +61,14 @@ for inputExcelFile in glob.iglob(path + "/Movements.xls"):
     csvFile["trxDate"] = pd.to_datetime(excelFile['F. VALOR'])
     csvFile["payee"] = excelFile['DESCRIPCIÓN'].apply(get_payee)
     csvFile["originalpayee"] = excelFile["DESCRIPCIÓN"].str.replace(",", "")
-    csvFile["amount"] = excelFile["IMPORTE (€)"].abs()
+    csvFile["amount"] = pd.to_numeric(excelFile["IMPORTE (€)"]).abs()
     csvFile["trxType"] = csvFile["amount"].apply(
         lambda x: "credit" if x >= 0 else "debit").astype('category')
     csvFile["category"] = ""
     csvFile["reference"] = ""
     csvFile["labels"] = accountName
-    csvFile['memo'] = excelFile[['CATEGORÍA', 'SUBCATEGORÍA']].\
-        agg('/'.join, axis=1) \
-        if excelFile["DESCRIPCIÓN"].str.startswith('Pago en') \
-        else ""
+    csvFile['memo'] = excelFile[['CATEGORÍA', 'SUBCATEGORÍA', "DESCRIPCIÓN"]] \
+        .apply(getDesc, axis=1)
 
     # adding to converted files list
     xlsList.append(csvFile)
